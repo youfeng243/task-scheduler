48fb6c6f7ee0d475e441ef0473ac7a6b5467d428
youfeng <youfeng@haizhi.com>
2017-08-03 16:29:59 +0800

    任务管理器工程创建

---
+++ check.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+project=task-scheduler
+main_class=com.haizhi.TaskScheduler
+
+check() {
+    pid=`ps -ef | grep java | grep "$1" | grep "$2" | awk '{print $2}'`
+    if [ -z "${pid}" ]; then
+        exit
+    fi
+
+    echo ${pid}
+}
+
+if [ $# == 2 ]; then
+    echo $(check $1 $2)
+else
+    echo $(check ${project} ${main_class})
+fi
---
+++ src/main/java/com/haizhi/TaskScheduler.java
@@ -0,0 +1,20 @@
+package com.haizhi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by youfeng on 2017/8/3.
+ * 任务管理器入口文件
+ */
+public class TaskScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskScheduler.class);
+
+    public static void main(String... args) {
+        logger.info("任务管理器开始执行...");
+
+
+        logger.info("任务管理器运行结束...");
+    }
+}
